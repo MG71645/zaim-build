8329528f7b3e8dc5ad3cfc904bf3967cd3cbd4e4
MG <mg71645@gmail.com>
2018-11-24 03:38:04 +0600

    Initial commit

---
+++ precache-manifest.a38c71c13544fea0c0bc708a7bb1805a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c413ff1bf9c2bf25faf1136205128cb",
+    "url": "/static/media/MuseoSansCyrl-300.7c413ff1.woff"
+  },
+  {
+    "revision": "d5476a86433b51754eaf",
+    "url": "/static/css/main.b41fdeb3.chunk.css"
+  },
+  {
+    "revision": "1b43d239fe2c73030f397743297ac071",
+    "url": "/static/media/MuseoSansCyrl-900.1b43d239.eot"
+  },
+  {
+    "revision": "58917f47d92df38aad58",
+    "url": "/static/js/1.58917f47.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c67b05570bc2d24c03962e07f37470f",
+    "url": "/static/media/MuseoSansCyrl-100.9c67b055.woff"
+  },
+  {
+    "revision": "0310003232b4b6424bffeac67e6fbed5",
+    "url": "/static/media/MuseoSansCyrl-100.03100032.ttf"
+  },
+  {
+    "revision": "c2e5987ab95e9df93393619a6eac8dea",
+    "url": "/static/media/MuseoSansCyrl-300.c2e5987a.ttf"
+  },
+  {
+    "revision": "a61eab68beb7bd4101cf05a752c7ac71",
+    "url": "/static/media/MuseoSansCyrl-100.a61eab68.eot"
+  },
+  {
+    "revision": "ac9e2c9a6153ec9ec9075194e2c3d6c6",
+    "url": "/static/media/MuseoSansCyrl-300.ac9e2c9a.eot"
+  },
+  {
+    "revision": "d5476a86433b51754eaf",
+    "url": "/static/js/main.d5476a86.chunk.js"
+  },
+  {
+    "revision": "2a4f9e378076126b9b14aae80b3b7133",
+    "url": "/static/media/MuseoSansCyrl-500.2a4f9e37.woff"
+  },
+  {
+    "revision": "0eb10fbab2e0bb9f53c3d632c491796e",
+    "url": "/static/media/MuseoSansCyrl-500.0eb10fba.eot"
+  },
+  {
+    "revision": "75a016c8d3ec2ec82e9cede574a5f33c",
+    "url": "/static/media/MuseoSansCyrl-700.75a016c8.ttf"
+  },
+  {
+    "revision": "473e321239aa0be55889a124daf6e0d3",
+    "url": "/static/media/MuseoSansCyrl-500.473e3212.ttf"
+  },
+  {
+    "revision": "a128eded3d8ae6f5e6cf2f9a2a106541",
+    "url": "/static/media/MuseoSansCyrl-700.a128eded.woff"
+  },
+  {
+    "revision": "ca722d99e2f0ed72746ec4ba56d866de",
+    "url": "/static/media/MuseoSansCyrl-700.ca722d99.eot"
+  },
+  {
+    "revision": "2d602d25c2d24b6f426083945c58a934",
+    "url": "/static/media/MuseoSansCyrl-900.2d602d25.woff"
+  },
+  {
+    "revision": "3b1501e4985e4d60dd05df6162795ee9",
+    "url": "/static/media/MuseoSansCyrl-900.3b1501e4.ttf"
+  },
+  {
+    "revision": "58917f47d92df38aad58",
+    "url": "/static/css/1.fa86bcc6.chunk.css"
+  },
+  {
+    "revision": "05a07d43603be6dc2ddc8005520b6cba",
+    "url": "/index.html"
+  }
+];
